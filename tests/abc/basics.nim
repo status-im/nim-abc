e1411ba79073f6dcc346331d576bd5df2730993a
Mark Spanbroek <mark@spanbroek.net>
2021-07-15 13:48:16 +0200

    Add questionable/results to basic imports

--- tests/abc/basics.nim
+++ tests/abc/basics.nim
@@ -1,10 +1,12 @@
 import std/unittest
 import pkg/questionable
+import pkg/questionable/results
 import abc
 import ./examples
 
 export unittest
 export questionable
+export results
 export abc
 export examples
 
