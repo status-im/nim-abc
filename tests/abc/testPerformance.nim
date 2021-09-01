206618c806dc339de91072a105c98182ef40ca81
Mark Spanbroek <mark@spanbroek.net>
2021-09-01 15:05:10 +0200

    Test performance of adding transaction to store

--- tests/abc/testPerformance.nim
+++ tests/abc/testPerformance.nim
@@ -1,8 +1,14 @@
 import std/times
 import std/strutils
+import abc/txstore
 import ./basics
+import ./alicebob
 
 suite "Performance":
+
+  let alice = PublicKey.alice
+  let bob = PublicKey.bob
+  let victor = PublicKey.victor
 
   template repeat(timespan, body): int =
     var count = 0
@@ -33,3 +39,24 @@ suite "Performance":
     let count = repeat(initDuration(milliseconds = 10)):
       wallet.sign(transaction)
     statistic "signatures per second", count * 100
+
+  proc generateTransactions(amount: int): seq[Transaction] =
+    var tx = Transaction.genesis
+    var payer = alice
+    var receiver = bob
+    for _ in 0..<amount:
+      tx = !Transaction.new({tx.hash: payer}, {receiver: 100.u256}, victor)
+      result.add(tx)
+      (payer, receiver) = (receiver, payer)
+
+  test "add transaction to store":
+    let transactions = generateTransactions(10_000)
+    var store = TxStore.new(Transaction.genesis)
+    var index = 0
+    let count = repeat(initDuration(milliseconds = 10)):
+      store.add(transactions[index])
+      inc index
+      if index == transactions.len:
+        index = 0
+        store = TxStore.new(Transaction.genesis)
+    statistic "transactions per second", count * 100
