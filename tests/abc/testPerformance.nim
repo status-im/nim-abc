1325a0c0afd19865c26c90f62f718eb1e4db8bdc
Mark Spanbroek <mark@spanbroek.net>
2021-09-02 10:56:35 +0200

    Measure performance of adding acks to a store

--- tests/abc/testPerformance.nim
+++ tests/abc/testPerformance.nim
@@ -58,3 +58,25 @@ suite "Performance":
         store.add(transactions[index])
         inc index
       statistic "transactions per second", count * 100
+
+  proc generateAcks(transactions: openArray[Transaction]): seq[Ack] =
+    var ack: ?Ack
+    for tx in transactions:
+      if previous =? ack:
+        ack = Ack.new(previous.hash, [tx.hash], victor)
+        result.add(!ack)
+      else:
+        ack = Ack.new([tx.hash], victor)
+        result.add(!ack)
+
+  test "add acknowledgement to store":
+    let transactions = generateTransactions(10_000)
+    let acks = generateAcks(transactions)
+    var store = TxStore.new(Transaction.genesis)
+    store.add(transactions)
+    var index = 0
+    for _ in 0..<4:
+      let count = repeat(initDuration(milliseconds = 10)):
+        store.add(acks[index])
+        inc index
+      statistic "acks per second", count * 100
